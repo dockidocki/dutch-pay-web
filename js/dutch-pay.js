434245e7819ecff06e68e48eb9f5602eca0120b8
Jayden <pyc8992dev@gmail.com>
2022-11-20 22:54:32 +0900

    절사 금액 지불자 선택 기능

--- js/dutch-pay.js
+++ js/dutch-pay.js
@@ -5,9 +5,20 @@ const peopleButton = dutchPayForm.querySelector("#people-btn");
 const peopleDiv = dutchPayForm.querySelector("#people");
 const isCutOffCheckBox = dutchPayForm.querySelector("#is-cut-off");
 const result = document.querySelector("#result");
+const resultTextDiv = document.querySelector("#result-text-div");
+const selectPayDiv = resultTextDiv.querySelector("#select-pay-div");
+
+const whoPaySelects = document.getElementsByClassName("select-pay");
+const whoPayDiv = document.querySelector("#who-pay-div");
+const whoPayButton = whoPayDiv.querySelector("#who-pay-btn");
+
+
+const HIDDEN_CLASS_NAME = "hidden";
+const ANOTHER_HIDDEN_CLASS_NAME = "hidden-another";
 
 let dutchPayObj = {};
 let resultText = null;
+let inputPeople = [];
 
 const onClickPeopleCount = () => {
     peopleDiv.innerText = '인원들의 이름을 입력해주세요';
@@ -41,48 +52,102 @@ const onSubmitCalculate = (e) => {
         `\n한 명은 ${dutchPayObj.financierAmount}원(절사 금액 포함 : ${dutchPayObj.cutOffDiffAmount}) 내야합니다` :
         '';
 
-    result.innerText = resultText;
+    resultTextDiv.innerText = resultText;
+    resultTextDiv.appendChild(selectPayDiv);
 
     if (isCutOff) {
-        let isRandomCheckBox = document.createElement('input');
-        isRandomCheckBox.id = 'is-random'
-        isRandomCheckBox.type = 'checkbox';
-        result.appendChild(isRandomCheckBox);
+        printSelectPay();
     }
     printPeoplePayTable();
 }
 
-// const onClickRandomFinancier = () => {
-//
-// }
+const printSelectPay = () => {
+    selectPayDiv.classList.remove(HIDDEN_CLASS_NAME);
+}
+
+const onClickSelectPay = (e) => {
+    if (e.target.value === 'another') {
+        whoPayDiv.classList.remove(ANOTHER_HIDDEN_CLASS_NAME);
+    } else {
+        whoPayDiv.classList.add(ANOTHER_HIDDEN_CLASS_NAME);
+    }
+}
+
+const onClickSelectPayPerson = () => {
+    let whoPayInput = whoPayDiv.querySelector("#who-pay");
+
+    for (let i = 0; i < inputPeople.length; i++) {
+        if (inputPeople[i].name === whoPayInput.value &&
+            inputPeople[i].isFinancier === false) {
+
+            for (let j = 0; j < inputPeople.length; j++) {
+                if (inputPeople[j].isFinancier === true) {
+                    inputPeople[j].payAmount = String(dutchPayObj.resultAmount);
+                    inputPeople[j].isFinancier = false;
+                    break;
+                }
+            }
+
+            inputPeople[i].payAmount = String(dutchPayObj.financierAmount);
+            inputPeople[i].isFinancier = true;
+
+            break;
+        }
+    }
+
+    printPeoplePayTable();
+}
 
 const printPeoplePayTable = () => {
     const peopleInputs = peopleDiv.getElementsByTagName('input');
 
-    result.appendChild(document.createElement('br'));
-    let tableElement = document.createElement('table');
-    result.appendChild(tableElement);
-    let header1 = document.createElement('th');
-    header1.innerText = '이름';
-    let header2 = document.createElement('th');
-    header2.innerText = '금액';
-    tableElement.appendChild(header1);
-    tableElement.appendChild(header2);
+    let resultTable = result.querySelector('#result-table');
 
-    for (let i = 0; i < peopleInputs.length; i++) {
-        tableElement.appendChild(document.createElement('tr'));
-        let td1 = document.createElement('td');
-        td1.innerText = peopleInputs[i].value;
-        tableElement.appendChild(td1);
-        let td2 = document.createElement('td');
-
-        // 물주 표기
-        if (i === 0) {
-            td2.innerText = String(dutchPayObj.financierAmount);
-        } else {
-            td2.innerText = String(dutchPayObj.resultAmount);
+    if (resultTable) {
+        let rows = resultTable.getElementsByTagName('tr');
+        for (let i = 0; i < inputPeople.length; i++) {
+            rows[i].querySelector('td:first-child').innerText = inputPeople[i].name;
+            rows[i].querySelector('td:last-child').innerText = inputPeople[i].payAmount;
         }
-        tableElement.appendChild(td2);
+    } else {
+        result.appendChild(document.createElement('br'));
+        let tableElement = document.createElement('table');
+        tableElement.id = 'result-table';
+        result.appendChild(tableElement);
+        let header1 = document.createElement('th');
+        header1.innerText = '이름';
+        let header2 = document.createElement('th');
+        header2.innerText = '금액';
+        tableElement.appendChild(header1);
+        tableElement.appendChild(header2);
+
+        for (let i = 0; i < peopleInputs.length; i++) {
+            let tr = document.createElement('tr');
+            tableElement.appendChild(tr);
+            let td1 = document.createElement('td');
+            td1.innerText = peopleInputs[i].value;
+            tr.appendChild(td1);
+            let td2 = document.createElement('td');
+
+            // 물주 표기
+            if (i === 0) {
+                td2.innerText = String(dutchPayObj.financierAmount);
+                inputPeople.push({
+                    name: peopleInputs[i].value,
+                    payAmount: String(dutchPayObj.financierAmount),
+                    isFinancier: true
+                });
+            } else {
+                td2.innerText = String(dutchPayObj.resultAmount);
+                inputPeople.push({
+                    name: peopleInputs[i].value,
+                    payAmount: String(dutchPayObj.resultAmount),
+                    isFinancier: false
+                });
+            }
+
+            tr.appendChild(td2);
+        }
     }
 }
 
@@ -116,3 +181,9 @@ const calculateDutchPay = (totalPay, peopleCount, cutOffAmount) => {
 
 dutchPayForm.addEventListener("submit", onSubmitCalculate);
 peopleButton.addEventListener("click", onClickPeopleCount)
+
+for (const select of whoPaySelects) {
+    select.addEventListener("click", onClickSelectPay);
+}
+
+whoPayButton.addEventListener("click", onClickSelectPayPerson);
